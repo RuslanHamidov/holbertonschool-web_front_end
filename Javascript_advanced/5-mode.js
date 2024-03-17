ffba49e83028cfef46b00a1af0dbad28363791cd
RuslanHamidov <rhamidov202@gmail.com>
2024-03-17 21:04:36 +0400

    add hogwarts

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -13,7 +13,26 @@ function main() {
     const darkMode = changeMode(12, "bold", "capitalize", "black", "white");
     const screamMode = changeMode(12, "normal", "lowercase", "white", "black");
     const body = document.body;
-    body.insertAdjacentHTML = ("beforebegin", "<p>aq</p>");
+    const paragraph = document.createElement('P');
+	paragraph.innerText = 'Welcome Holberton!';
+	body.appendChild(paragraph);
+
+	const buttonSpooky = document.createElement('BUTTON');
+	buttonSpooky.innerText = 'Spooky';
+	const buttonDark = document.createElement('BUTTON');
+	buttonDark.innerText = 'Dark mode';
+	const buttonScream = document.createElement('BUTTON');
+	buttonScream.innerText = 'Scream mode';
+
+	body.appendChild(buttonSpooky);
+	body.appendChild(buttonDark);
+	body.appendChild(buttonScream);
+
+	buttonSpooky.onclick = spooky;
+	buttonDark.onclick = darkMode;
+	buttonScream.onclick = screamMode;
+
+
 }
 
 main();
